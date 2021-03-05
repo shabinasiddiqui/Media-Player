b2a78b29799caacbe91051516aead88d2434face
Shabina Siddiqui <siddiquishabina10@gmail.com>
2021-03-05 09:38:53 +0530

    added menu

--- mediaplayer.py
+++ mediaplayer.py
@@ -1,10 +1,40 @@
-from PyQt5.QtGui import QIcon, QPalette,QKeySequence
-from PyQt5.QtWidgets import QApplication,QWidget,QHBoxLayout,QPushButton,QVBoxLayout,QLabel,QSlider,QStyle, QSizePolicy,QFileDialog,QShortcut,QMenu, QAction,QMenuBar,QLineEdit
+from PyQt5.QtGui import QIcon, QKeyEvent, QPalette,QKeySequence
+from PyQt5.QtWidgets import QApplication,QWidget,QHBoxLayout,QPushButton,QVBoxLayout,QDialog,QLabel,QSlider,QStyle, QSizePolicy,QFileDialog,QShortcut,QMenu, QAction,QMenuBar,QLineEdit
 import sys 
 from PyQt5.QtMultimedia import QMediaPlayer,QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt,QUrl,QTime
+
+class InfoDialog(QDialog):
+  def __init__(self, parent=None):
+    super().__init__(parent=parent)
+
+    self.setWindowTitle("Info (shortcuts)")
+
+    self.layout = QVBoxLayout()
+    m1 = QLabel("FullScreen     -  F / Double Click")
+    m2 = QLabel("Close App      -  Ctrl + Q")
+    m3 = QLabel("Open File      -  Ctrl + O")
+    m4 = QLabel("Hide toggle    -  H")
+    m5 = QLabel("Forward 10sec  -  →")
+    m6 = QLabel("Backward 10sec -  ←")
+    m7 = QLabel("Forward 1min   -  Shift + →")
+    m8 = QLabel("Backward 1min  -  Shift + ←")
+    m9 = QLabel("Volume UP      -  ↑")
+    m10 = QLabel("Volume UP     -  ↓")
+    
+    self.layout.addWidget(m1)
+    self.layout.addWidget(m2)
+    self.layout.addWidget(m3)
+    self.layout.addWidget(m4)
+    self.layout.addWidget(m5)
+    self.layout.addWidget(m6)
+    self.layout.addWidget(m7)
+    self.layout.addWidget(m8)
+    self.layout.addWidget(m9)
+    self.layout.addWidget(m10)
+    self.setLayout(self.layout)
 
 class Window(QWidget):
   def __init__(self, parent=None):
@@ -27,7 +57,7 @@ class Window(QWidget):
     self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
     # create videowidget object
-    videowidget = QVideoWidget()
+    self.videowidget = QVideoWidget()
 
     # create open button
     self.openbtn = QPushButton('Open Video', self)  
@@ -38,12 +68,63 @@ class Window(QWidget):
     self.openbtn.setStyleSheet('background-color : black ; color : white')
     self.openbtn.clicked.connect(self.open_file)
 
+    # media Menu
+
+    self.mediabtn = QMenuBar()
+    actionFile = self.mediabtn.addMenu(QIcon('icons/media.png'),'Media')
+    self.mediabtn.setStyleSheet('background-color : black; color : white')
+    actionFile.addSeparator()
+
+    PlaybackMenu = QMenu('Playback', self)
+    Forward10 = QAction('Forward 10sec', self)
+    PlaybackMenu.addAction(Forward10)
+    Forward10.triggered.connect(self.forwardSlider)
+    Forward1 = QAction('Forward 1min', self)
+    PlaybackMenu.addAction(Forward1)
+    Forward1.triggered.connect(self.forwardSlider10)    
+    Backward10 = QAction('Backward 10sec', self)
+    PlaybackMenu.addAction(Backward10)
+    Backward10.triggered.connect(self.backSlider)
+    Backward1 = QAction('Backward 1min', self)
+    PlaybackMenu.addAction(Backward1)
+    Backward1.triggered.connect(self.backSlider10)
+
+    VolumeIncrease = QAction(QIcon('icons/max.png'),'Volume Increase', self)
+    actionFile.addAction(VolumeIncrease)
+    VolumeIncrease.triggered.connect(self.volumeUp)
+    VolumeDecrease = QAction(QIcon('icons/min.png'),'Volume Decrease', self)
+    actionFile.addAction(VolumeDecrease)
+    VolumeDecrease.triggered.connect(self.volumeDown)
+    VolumeMute = QAction(QIcon('icons/mute.png'),'Volume Mute', self)
+    actionFile.addAction(VolumeMute)
+    VolumeMute.triggered.connect(self.volumeMute)
+
+    while self.mediabtn.is_pressed():
+      if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+        Play = QAction(QIcon('icons/pause.png'),'Pause', self)
+        actionFile.addAction(Play)
+        Play.triggered.connect(self.play_video)
+      else:
+        Pause = QAction(QIcon('icons/play.png'),'Play', self)
+        actionFile.addAction(Pause)
+        Pause.triggered.connect(self.play_video)
+
+    actionFile.addMenu(PlaybackMenu)
+    PlaybackMenu.setStyleSheet('background-color : black; color : white')
+    
+    self.mediabtn.setFixedHeight(25)
+    self.mediabtn.setMaximumWidth(200)
+    self.mediabtn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
+
+    # Display menu                                                         
 
     self.displaybtn = QMenuBar()
     actionFile = self.displaybtn.addMenu(QIcon('icons/display.png'),'Setting')
     self.displaybtn.setStyleSheet('background-color : black; color : white')
-    actionFile.addAction("Full Screen")
+    full = QAction('Full Screen', self)
+    actionFile.addAction(full)
     actionFile.addSeparator()
+
     AspectMenu = QMenu('Aspect Ratio', self)
     Aspect169 = QAction('16:9', self)
     AspectMenu.addAction(Aspect169)
@@ -65,14 +146,16 @@ class Window(QWidget):
     actionFile = self.settingbtn.addMenu(QIcon('icons/settings.png'),'Setting')
     self.settingbtn.setStyleSheet('background-color : black; color : white')
     actionFile.addAction("New")
-    actionFile.addAction("Open")
-    actionFile.addAction("Save")
     actionFile.addSeparator()
     exit = QAction(QIcon('icons/exit.png'), 'Exit', self)
     exit.setShortcut('Ctrl+Q')
-    exit.setStatusTip('Exit application')
     exit.triggered.connect(app.quit)
     actionFile.addAction(exit) 
+    actionFile.addSeparator()
+    info = QAction(QIcon('icons/info.png'), 'Info', self)
+    info.setShortcut('F12')
+    info.triggered.connect(self.open_info_dialog)
+    actionFile.addAction(info)
     
 
     self.settingbtn.setFixedHeight(25)
@@ -145,8 +228,10 @@ class Window(QWidget):
     self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
 
     #create vol label
-    self.vlabel = QLabel(self)
-    self.vlabel.setPixmap(QPixmap('icons/mute.png'))
+    self.vlabel = QPushButton(self)
+    self.vlabel.setIcon(QIcon('icons/mute.png'))
+    self.vlabel.setStyleSheet('background-color : black')
+    self.vlabel.clicked.connect(self.volumeMute)
 
     # create hbox layout
     hboxLayout = QHBoxLayout()
@@ -167,26 +252,27 @@ class Window(QWidget):
     tboxLayout=QHBoxLayout()
     tboxLayout.setAlignment(Qt.AlignLeft)
     tboxLayout.addWidget(self.openbtn)
+    tboxLayout.addWidget(self.mediabtn)
     tboxLayout.addWidget(self.displaybtn)
     tboxLayout.addWidget(self.settingbtn)
   
     #create vbox layout 
     vboxlayout = QVBoxLayout()
     vboxlayout.addLayout(tboxLayout)
-    vboxlayout.addWidget(videowidget)
+    vboxlayout.addWidget(self.videowidget)
     vboxlayout.addLayout(hboxLayout)
     vboxlayout.addWidget(self.label)
     self.setLayout (vboxlayout)
-    self.mediaPlayer.setVideoOutput(videowidget)
+    self.mediaPlayer.setVideoOutput(self.videowidget)
 
     # media player signals
 
     self.mediaPlayer.stateChanged.connect(self.mediastate_changed)
     self.mediaPlayer.positionChanged.connect(self.position_changed)
     self.mediaPlayer.durationChanged.connect(self.duration_changed)
-    self.mediaPlayer.positionChanged.connect(self.volume_changed)  
+    # self.mediaPlayer.positionChanged.connect(self.volume_changed)  
 
-    self.widescreen = True
+    self.videowidget = True
       
     #shortcuts
     self.shortcut = QShortcut(QKeySequence('Ctrl+O'), self)
@@ -208,6 +294,12 @@ class Window(QWidget):
 
 
   # function section    
+  
+  def open_info_dialog(self):
+
+    info = InfoDialog()
+    info.exec_()
+  
   def mouseDoubleClickEvent(self, event):
     self.handleFullscreen()
 
@@ -258,28 +350,38 @@ class Window(QWidget):
   def set_volume(self, position):
     self.mediaPlayer.setVolume(position)
   
-  def volume_changed(self, position):
-    self.sld.setValue(position)
+  # def volume_changed(self, position):
+  #   self.sld.setValue(position)
 
   def changeValue(self, value):
-
-    self.volume = value
-    if self.volume == 0:
-      self.vlabel.setPixmap(QPixmap('icons/mute.png'))
-    elif 0 < self.volume <= 30:
-      self.vlabel.setPixmap(QPixmap('icons/min.png'))
-    elif 30 < self.volume < 80:
-      self.vlabel.setPixmap(QPixmap('icons/med.png'))
+    if self.mediaPlayer.isMuted():
+      self.vlabel.setIcon(QIcon('icons/mute.png'))
     else:
-      self.vlabel.setPixmap(QPixmap('icons/max.png'))
+      self.volume = value
+      if self.volume == 0:
+        self.vlabel.setIcon(QIcon('icons/mute.png'))
+      elif 0 < self.volume <= 30:
+        self.vlabel.setIcon(QIcon('icons/min.png'))
+      elif 30 < self.volume < 80:
+        self.vlabel.setIcon(QIcon('icons/med.png'))
+      else:
+        self.vlabel.setIcon(QIcon('icons/max.png'))
 
+  def volumeMute(self):
+    if self.mediaPlayer.isMuted():
+      self.mediaPlayer.setMuted(False)
+      self.mediaPlayer.setVolume(100)
+      self.vlabel.setIcon(QIcon('icons/max.png'))
+    else:
+      self.mediaPlayer.setMuted(True)
+      self.vlabel.setIcon(QIcon('icons/mute.png'))
 
   def volumeUp(self):
-    vol= self.mediaPlayer.setVolume(self.mediaPlayer.volume() + 10)
+    self.mediaPlayer.setVolume(self.mediaPlayer.volume() + 10)
     print("Volume: " + str(self.mediaPlayer.volume()))
     
   def volumeDown(self):
-    vol=self.mediaPlayer.setVolume(self.mediaPlayer.volume() - 10)    
+    self.mediaPlayer.setVolume(self.mediaPlayer.volume() - 10)  
     print("Volume: " + str(self.mediaPlayer.volume()))
 
   def forwardSlider(self):
@@ -308,7 +410,7 @@ class Window(QWidget):
       print("Fullscreen entered")
 
   def screen169(self):
-    self.widescreen = True
+    self.videowidget = True
     mwidth = self.frameGeometry().width()
     mheight = self.frameGeometry().height()
     mleft = self.frameGeometry().left()
@@ -317,7 +419,7 @@ class Window(QWidget):
     self.setGeometry(mleft, mtop, mwidth, round(mwidth / mratio))
 
   def screen43(self):
-    self.widescreen = False
+    self.videowidget = False
     mwidth = self.frameGeometry().width()
     mheight = self.frameGeometry().height()
     mleft = self.frameGeometry().left()
@@ -348,8 +450,8 @@ class Window(QWidget):
   
   def showSlider(self):
     self.openbtn.show()
-    self.settingbtn.hide()
-    self.displaybtn.hide()
+    self.settingbtn.show()
+    self.displaybtn.show()
     self.bbtn.show()
     self.playbtn.show()
     self.fbtn.show()
